18e3a1907a8db5ec4e67e8376d9885d8bb74b833
Shreyam Hemanta <shreyam2014@gmail.com>
2023-12-08 22:44:16 +0530

    commit

--- creditCardFraud.py
+++ creditCardFraud.py
@@ -3,9 +3,8 @@ from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.metrics import accuracy_score, mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor
 
 file = pd.read_csv("/Users/shreyamhemanta/Documents/GitHub/Credit-Card-Fraud-Detection-2023-model/creditcard_2023.csv")
 
@@ -66,44 +65,86 @@ y_pred_min_max_scaled_log = log_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_log = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_log)*100
 print(y_pred_min_max_scaled_log, "\n", acc_min_max_scaled_log)
 
-print("\nUnscaled Decision Tree")
+print("\nUnscaled Decision Tree Classifier")
 dt = DecisionTreeClassifier()
 dt.fit(x_train, y_train)
 y_pred_dt = dt.predict(x_test)
 acc_dt = accuracy_score(y_test, y_pred_dt)*100
 print(y_pred_dt, "\n", acc_dt)
 
-print("\nStandard Scaled Decision Tree")
+print("\nStandard Scaled Decision Tree Classifier")
 dt_scaled = DecisionTreeClassifier()
 dt_scaled.fit(x_train_scaled, y_train_scaled)
 y_pred_scaled_dt = dt_scaled.predict(x_test_scaled)
 acc_scaled_dt = accuracy_score(y_test_scaled, y_pred_scaled_dt)*100
 print(y_pred_scaled_dt, "\n", acc_scaled_dt)
 
-print("\nMin-Max Scaled Decision Tree")
+print("\nMin-Max Scaled Decision Tree Classifier")
 dt_min_max_scaled = DecisionTreeClassifier()
 dt_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
 y_pred_min_max_scaled_dt = dt_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_dt = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_dt)*100
 print(y_pred_min_max_scaled_dt, "\n", acc_min_max_scaled_dt)
 
-print("\nUnscaled Random Forest")
+print("\nUnscaled Decision Tree Regressor")
+dt_reg = DecisionTreeRegressor()
+dt_reg.fit(x_train, y_train)
+y_pred_dt_reg = dt_reg.predict(x_test)
+acc_dt_reg = accuracy_score(y_test, y_pred_dt_reg)*100
+print(y_pred_dt_reg, "\n", acc_dt_reg)
+
+print("\nStandard Scaled Decision Tree Regressor")
+dt_reg_scaled = DecisionTreeRegressor()
+dt_reg_scaled.fit(x_train_scaled, y_train_scaled)
+y_pred_scaled_dt_reg = dt_reg_scaled.predict(x_test_scaled)
+acc_scaled_dt_reg = accuracy_score(y_test_scaled, y_pred_scaled_dt_reg)*100
+print(y_pred_scaled_dt_reg, "\n", acc_scaled_dt_reg)
+
+print("\nMin-Max Scaled Decision Tree Regressor")
+dt_reg_min_max_scaled = DecisionTreeRegressor()
+dt_reg_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
+y_pred_min_max_scaled_dt_reg = dt_reg_min_max_scaled.predict(x_test_min_max_scaled)
+acc_min_max_scaled_dt_reg = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_dt_reg)*100
+print(y_pred_min_max_scaled_dt_reg, "\n", acc_min_max_scaled_dt_reg)
+
+print("\nUnscaled Random Forest Classifier")
 rf = RandomForestClassifier()
 rf.fit(x_train, y_train)
 y_pred_rf = rf.predict(x_test)
 acc_rf = accuracy_score(y_test, y_pred_rf)*100
 print(y_pred_rf, "\n", acc_rf)
 
-print("\nStandard Scaled Random Forest")
-rf_scaled = DecisionTreeClassifier()
+print("\nStandard Scaled Random Forest Classifier")
+rf_scaled = RandomForestClassifier()
 rf_scaled.fit(x_train_scaled, y_train_scaled)
 y_pred_scaled_rf = rf_scaled.predict(x_test_scaled)
 acc_scaled_rf = accuracy_score(y_test_scaled, y_pred_scaled_rf)*100
 print(y_pred_scaled_rf, "\n", acc_scaled_rf)
 
-print("\nMin-Max Scaled Random Forest")
-rf_min_max_scaled = DecisionTreeClassifier()
+print("\nMin-Max Scaled Random Forest Classifier")
+rf_min_max_scaled = RandomForestClassifier()
 rf_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
 y_pred_min_max_scaled_rf = rf_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_rf = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_rf)*100
 print(y_pred_min_max_scaled_rf, "\n", acc_min_max_scaled_rf)
+
+print("\nUnscaled Random Forest Regressor")
+rf_reg = RandomForestRegressor()
+rf_reg.fit(x_train, y_train)
+y_pred_rf_reg = rf_reg.predict(x_test)
+acc_rf_reg = accuracy_score(y_test, y_pred_rf_reg)*100
+print(y_pred_rf_reg, "\n", acc_rf_reg)
+
+print("\nStandard Scaled Random Forest Regressor")
+rf_reg_scaled = RandomForestRegressor()
+rf_reg_scaled.fit(x_train_scaled, y_train_scaled)
+y_pred_scaled_rf_reg = rf_reg_scaled.predict(x_test_scaled)
+acc_scaled_rf_reg = accuracy_score(y_test_scaled, y_pred_scaled_rf_reg)*100
+print(y_pred_scaled_rf_reg, "\n", acc_scaled_rf_reg)
+
+print("\nMin-Max Scaled Random Forest Regressor")
+rf_reg_min_max_scaled = RandomForestRegressor()
+rf_reg_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
+y_pred_min_max_scaled_rf_reg = rf_reg_min_max_scaled.predict(x_test_min_max_scaled)
+acc_min_max_scaled_rf_reg = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_rf_reg)*100
+print(y_pred_min_max_scaled_rf_reg, "\n", acc_min_max_scaled_rf_reg)
--- new_test_file.py
+++ new_test_file.py
@@ -148,14 +148,3 @@ rf_reg_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
 y_pred_min_max_scaled_rf_reg = rf_reg_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_rf_reg = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_rf_reg)*100
 print(y_pred_min_max_scaled_rf_reg, "\n", acc_min_max_scaled_rf_reg)
-
-# # Plot the Linear Regression results
-# plt.figure(figsize=(10, 6))
-# plt.scatter(y_test, y_pred_lin, label='Actual vs Predicted (Unscaled)')
-# plt.scatter(y_test, y_pred_scaled_lin, label='Actual vs Predicted (Standard Scaled)')
-# plt.scatter(y_test, y_pred_min_max_scaled_lin, label='Actual vs Predicted (Min-Max Scaled)')
-# plt.xlabel('Actual Values')
-# plt.ylabel('Predicted Values')
-# plt.legend()
-# plt.title('Linear Regression Results')
-# plt.show()
