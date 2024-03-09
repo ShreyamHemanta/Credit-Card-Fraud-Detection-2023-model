998059c463e26e3ed8ef20c8d65932cd12ed4ba3
Shreyam Hemanta <shreyam2014@gmail.com>
2024-03-09 20:30:55 +0530

    commit

--- creditCardFraud.py
+++ creditCardFraud.py
@@ -23,128 +23,128 @@ mm = MinMaxScaler()
 x_min_max_scaled = mm.fit_transform(x)
 x_train_min_max_scaled, x_test_min_max_scaled, y_train_min_max_scaled, y_test_min_max_scaled = train_test_split(x_min_max_scaled, y, train_size=0.33, random_state=42)
 
-print("\nUnscaled Linear Regression")
+print("\nUnscaled Linear Regression :")
 lin = LinearRegression()
 lin.fit(x_train, y_train)
 y_pred_lin = lin.predict(x_test)
 acc_lin = (1 - mean_absolute_error(y_test, y_pred_lin))*100
-print(y_pred_lin, "\n", acc_lin)
+print(y_pred_lin, "\nAccuracy : ", acc_lin)
 
-print("\nStandard Scaled Linear Regression")
+print("\nStandard Scaled Linear Regression :")
 lin_scaled = LinearRegression()
 lin_scaled.fit(x_train_scaled, y_train_scaled)
 y_pred_scaled_lin = lin_scaled.predict(x_test_scaled)
 acc_scaled_lin = (1 - mean_absolute_error(y_test_scaled, y_pred_scaled_lin))*100
-print(y_pred_scaled_lin, "\n", acc_scaled_lin)
+print(y_pred_scaled_lin, "\nAccuracy : ", acc_scaled_lin)
 
-print("\nMin-Max Scaled Linear Regression")
+print("\nMin-Max Scaled Linear Regression :")
 lin_min_max_scaled = LinearRegression()
 lin_min_max_scaled.fit(x_train_min_max_scaled,y_train_min_max_scaled)
 y_pred_min_max_scaled_lin = lin_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_lin = (1 - mean_absolute_error(y_test_min_max_scaled, y_pred_min_max_scaled_lin))*100
-print(y_pred_min_max_scaled_lin, "\n", acc_min_max_scaled_lin)
+print(y_pred_min_max_scaled_lin, "\nAccuracy : ", acc_min_max_scaled_lin)
 
-print("\nUnscaled Logistic Regression")
+print("\nUnscaled Logistic Regression :")
 log = LogisticRegression()
 log.fit(x_train, y_train)
 y_pred_log = log.predict(x_test)
 acc_log = accuracy_score(y_test, y_pred_log)*100
-print(y_pred_lin, "\n", acc_log)
+print(y_pred_lin, "\nAccuracy : ", acc_log)
 
-print("\nStandard Scaled Logistic Regression")
+print("\nStandard Scaled Logistic Regression :")
 log_scaled = LogisticRegression()
 log_scaled.fit(x_train_scaled, y_train_scaled)
 y_pred_scaled_log = log_scaled.predict(x_test_scaled)
 acc_scaled_log = accuracy_score(y_test_scaled, y_pred_scaled_log)*100
-print(y_pred_scaled_log, "\n", acc_scaled_log)
+print(y_pred_scaled_log, "\nAccuracy : ", acc_scaled_log)
 
-print("\nMin-Max Scaled Logistic Regression")
+print("\nMin-Max Scaled Logistic Regression :")
 log_min_max_scaled = LogisticRegression()
 log_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
 y_pred_min_max_scaled_log = log_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_log = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_log)*100
-print(y_pred_min_max_scaled_log, "\n", acc_min_max_scaled_log)
+print(y_pred_min_max_scaled_log, "\nAccuracy : ", acc_min_max_scaled_log)
 
-print("\nUnscaled Decision Tree Classifier")
+print("\nUnscaled Decision Tree Classifier :")
 dt = DecisionTreeClassifier()
 dt.fit(x_train, y_train)
 y_pred_dt = dt.predict(x_test)
 acc_dt = accuracy_score(y_test, y_pred_dt)*100
-print(y_pred_dt, "\n", acc_dt)
+print(y_pred_dt, "\nAccuracy : ", acc_dt)
 
-print("\nStandard Scaled Decision Tree Classifier")
+print("\nStandard Scaled Decision Tree Classifier :")
 dt_scaled = DecisionTreeClassifier()
 dt_scaled.fit(x_train_scaled, y_train_scaled)
 y_pred_scaled_dt = dt_scaled.predict(x_test_scaled)
 acc_scaled_dt = accuracy_score(y_test_scaled, y_pred_scaled_dt)*100
-print(y_pred_scaled_dt, "\n", acc_scaled_dt)
+print(y_pred_scaled_dt, "\nAccuracy : ", acc_scaled_dt)
 
-print("\nMin-Max Scaled Decision Tree Classifier")
+print("\nMin-Max Scaled Decision Tree Classifier :")
 dt_min_max_scaled = DecisionTreeClassifier()
 dt_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
 y_pred_min_max_scaled_dt = dt_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_dt = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_dt)*100
-print(y_pred_min_max_scaled_dt, "\n", acc_min_max_scaled_dt)
+print(y_pred_min_max_scaled_dt, "\nAccuracy : ", acc_min_max_scaled_dt)
 
-print("\nUnscaled Decision Tree Regressor")
+print("\nUnscaled Decision Tree Regressor :")
 dt_reg = DecisionTreeRegressor()
 dt_reg.fit(x_train, y_train)
 y_pred_dt_reg = dt_reg.predict(x_test)
 acc_dt_reg = accuracy_score(y_test, y_pred_dt_reg)*100
-print(y_pred_dt_reg, "\n", acc_dt_reg)
+print(y_pred_dt_reg, "\nAccuracy : ", acc_dt_reg)
 
-print("\nStandard Scaled Decision Tree Regressor")
+print("\nStandard Scaled Decision Tree Regressor :")
 dt_reg_scaled = DecisionTreeRegressor()
 dt_reg_scaled.fit(x_train_scaled, y_train_scaled)
 y_pred_scaled_dt_reg = dt_reg_scaled.predict(x_test_scaled)
 acc_scaled_dt_reg = accuracy_score(y_test_scaled, y_pred_scaled_dt_reg)*100
-print(y_pred_scaled_dt_reg, "\n", acc_scaled_dt_reg)
+print(y_pred_scaled_dt_reg, "\nAccuracy : ", acc_scaled_dt_reg)
 
-print("\nMin-Max Scaled Decision Tree Regressor")
+print("\nMin-Max Scaled Decision Tree Regressor :")
 dt_reg_min_max_scaled = DecisionTreeRegressor()
 dt_reg_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
 y_pred_min_max_scaled_dt_reg = dt_reg_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_dt_reg = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_dt_reg)*100
-print(y_pred_min_max_scaled_dt_reg, "\n", acc_min_max_scaled_dt_reg)
+print(y_pred_min_max_scaled_dt_reg, "\nAccuracy : ", acc_min_max_scaled_dt_reg)
 
-print("\nUnscaled Random Forest Classifier")
+print("\nUnscaled Random Forest Classifier :")
 rf = RandomForestClassifier()
 rf.fit(x_train, y_train)
 y_pred_rf = rf.predict(x_test)
 acc_rf = accuracy_score(y_test, y_pred_rf)*100
-print(y_pred_rf, "\n", acc_rf)
+print(y_pred_rf, "\nAccuracy : ", acc_rf)
 
-print("\nStandard Scaled Random Forest Classifier")
+print("\nStandard Scaled Random Forest Classifier :")
 rf_scaled = RandomForestClassifier()
 rf_scaled.fit(x_train_scaled, y_train_scaled)
 y_pred_scaled_rf = rf_scaled.predict(x_test_scaled)
 acc_scaled_rf = accuracy_score(y_test_scaled, y_pred_scaled_rf)*100
-print(y_pred_scaled_rf, "\n", acc_scaled_rf)
+print(y_pred_scaled_rf, "\nAccuracy : ", acc_scaled_rf)
 
-print("\nMin-Max Scaled Random Forest Classifier")
+print("\nMin-Max Scaled Random Forest Classifier :")
 rf_min_max_scaled = RandomForestClassifier()
 rf_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
 y_pred_min_max_scaled_rf = rf_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_rf = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_rf)*100
-print(y_pred_min_max_scaled_rf, "\n", acc_min_max_scaled_rf)
+print(y_pred_min_max_scaled_rf, "\nAccuracy : ", acc_min_max_scaled_rf)
 
-print("\nUnscaled Random Forest Regressor")
+print("\nUnscaled Random Forest Regressor :")
 rf_reg = RandomForestRegressor()
 rf_reg.fit(x_train, y_train)
 y_pred_rf_reg = rf_reg.predict(x_test)
 acc_rf_reg = accuracy_score(y_test, y_pred_rf_reg)*100
-print(y_pred_rf_reg, "\n", acc_rf_reg)
+print(y_pred_rf_reg, "\nAccuracy : ", acc_rf_reg)
 
-print("\nStandard Scaled Random Forest Regressor")
+print("\nStandard Scaled Random Forest Regressor :")
 rf_reg_scaled = RandomForestRegressor()
 rf_reg_scaled.fit(x_train_scaled, y_train_scaled)
 y_pred_scaled_rf_reg = rf_reg_scaled.predict(x_test_scaled)
 acc_scaled_rf_reg = accuracy_score(y_test_scaled, y_pred_scaled_rf_reg)*100
-print(y_pred_scaled_rf_reg, "\n", acc_scaled_rf_reg)
+print(y_pred_scaled_rf_reg, "\nAccuracy : ", acc_scaled_rf_reg)
 
-print("\nMin-Max Scaled Random Forest Regressor")
+print("\nMin-Max Scaled Random Forest Regressor :")
 rf_reg_min_max_scaled = RandomForestRegressor()
 rf_reg_min_max_scaled.fit(x_train_min_max_scaled, y_train_min_max_scaled)
 y_pred_min_max_scaled_rf_reg = rf_reg_min_max_scaled.predict(x_test_min_max_scaled)
 acc_min_max_scaled_rf_reg = accuracy_score(y_test_min_max_scaled, y_pred_min_max_scaled_rf_reg)*100
-print(y_pred_min_max_scaled_rf_reg, "\n", acc_min_max_scaled_rf_reg)
+print(y_pred_min_max_scaled_rf_reg, "\nAccuracy : ", acc_min_max_scaled_rf_reg)
